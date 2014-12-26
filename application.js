a85772fdd094c99d7b52ebceebac0ccc22b1a176
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2014-12-26 00:02:02 -0500

    1. added react todo with adding new item 2. need to do removing item

--- application.js
+++ application.js
@@ -11,48 +11,64 @@ var Todo = Backbone.Collection.extend({
   model: TodoItem
 });
 
-var App = Backbone.View.extend({
-  tagName: 'ul',
-  initialize: function(){
-    var todo = new Todo([
-      {name: 'study', isDone: false},
-      {name: 'bath', isDone: false},
-      {name: 'eat', isDone: false}
-    ]);
-    todo.forEach(this.renderAll, this);
-  },
-  render: function(){
-    return this.$el;
-  },
-  renderAll: function(model){
-    var todoItemView = new TodoItem({model: model.toJSON()});
-    this.$el.append(todoItemView.el);
-  }
-});
+// var App = Backbone.View.extend({
+//   tagName: 'ul',
+//   initialize: function(){
+//     var todo = new Todo([
+//       {name: 'study', isDone: false},
+//       {name: 'bath', isDone: false},
+//       {name: 'eat', isDone: false}
+//     ]);
+//     todo.forEach(this.renderAll, this);
+//   },
+//   render: function(){
+//     return this.$el;
+//   },
+//   renderAll: function(model){
+//     var todoItemView = new TodoItem({model: model.toJSON()});
+//     this.$el.append(todoItemView.el);
+//   }
+// });
 
-var TodoItem = Backbone.View.extend({
-  tagName: 'li',
-  template: _.template('<%=name%>: <%=isDone%>'),
-  initialize: function(){
-    this.render();
-  },
-  render: function(){
-    this.$el.html(this.template(this.model));
-  }
-})
-var app = new App();
-$('#b').html(app.render());
+// var TodoItem = Backbone.View.extend({
+//   tagName: 'li',
+//   template: _.template('<%=name%>: <%=isDone%>'),
+//   initialize: function(){
+//     this.render();
+//   },
+//   render: function(){
+//     this.$el.html(this.template(this.model));
+//   }
+// })
+// var app = new App();
+// $('#backbone').html(app.render());
 
-var todoList = new Todo([
-      {name: 'study', isDone: false},
-      {name: 'bath', isDone: false},
-      {name: 'eat', isDone: false}
-]);
+/*
+[
+  {name: 'study', isDone: false},
+  {name: 'bath', isDone: false},
+  {name: 'eat', isDone: false}
+]
+*/
 
+var myLists = [];
+if(!window.localStorage.getItem("myLists")){
+  window.localStorage.setItem("myLists", JSON.stringify(myLists));
+}else{
+  myLists = JSON.parse(window.localStorage.getItem("myLists"));
+}
+
+var todoList = new Todo(myLists);
 var TodoItemComponent = React.createClass({
+  deleteItem: function(){
+    this.setState(this.props.todoItem.toJSON());
+    todoList.remove(this.props.todoItem);
+    console.log(todoList);
+    console.log(this.props.todoItem.toJSON());
+  },
   render: function(){
     return (
-      <li>
+      <li onClick={this.deleteItem}>
         {this.props.todoItem.get("name")}: {this.props.todoItem.get("isDone").toString()} 
       </li>
     );
@@ -60,20 +76,35 @@ var TodoItemComponent = React.createClass({
 })
 
 var TodoListComponent = React.createClass({
+  enter: function(e){
+    var inp = this.refs.input.getDOMNode().value;
+    if(e.keyCode === 13 && inp.trim()){
+      var item = {name: inp, isDone: false};
+      this.setState(item);
+      var nn = new TodoItem(item);
+      todoList.add(nn);
+      myLists.push(nn.toJSON());
+      window.localStorage.setItem("myLists", JSON.stringify(myLists));
+      this.refs.input.getDOMNode().value = '';
+    }
+  },
   render: function(){
     var items = [];
     this.props.todoList.forEach(function(item){
-          items.push(<TodoItemComponent todoItem={item}/>);
-        });
+      items.push(<TodoItemComponent todoItem={item}/>);
+    });
     return (
-      <ul className="todo-list">
-        {items}
-      </ul>
+      <div>
+        <ul className="todo-list">
+          {items}
+        </ul>
+        <input type="text" placeholder="Enter new item" onKeyDown={this.enter} ref="input"/>
+      </div>
     );
   }
 });
 
 React.render(
   <TodoListComponent todoList={todoList} />,
-  document.getElementById('r')
+  document.getElementById('react')
 );
